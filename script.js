8f686e47d3a02b73476f23d1fb29b12edd9dbe77
João Lucas da Silva Maciel <joao162002009@gmail.com>
2023-06-07 10:04:56 -0300

    Arquivos
    Arquivos atualizados

---
+++ script.js
@@ -0,0 +1,10 @@
+function moveNoButton() {
+    var button = document.getElementById("no-button");
+    button.style.position = "absolute";
+    button.style.left = Math.random() * 90 + "%";
+    button.style.top = Math.random() * 90 + "%";
+}
+
+function openGif() {
+    window.open("https://makeagif.com/i/luyI9E");
+}
